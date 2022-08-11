3eaa236c9e0e4c426d3a6d0b74e8f35ec220b6f1
Adam Harris <adam_harris@hotmail.com>
2022-08-11 15:22:11 +0100

    Resource node changes sprite as it is damaged

--- TimothEUnified/Assets/_Scripts/ResourceNode.cs
+++ TimothEUnified/Assets/_Scripts/ResourceNode.cs
@@ -18,14 +18,24 @@ public class ResourceNode : MonoBehaviour
 
     [SerializeField] int _amount = 1;
 
+    int _currentSpriteIndex = 0;
+
+    [SerializeField] Sprite[] _sprites;
     [SerializeField] float _minimumToolPower = 30.0f;
     [SerializeField] ToolType _typeToDestroy;
 
     float _currentHealth;
+    SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Start()
     {
         _currentHealth = _nodeMaxHealth;
+        _renderer.sprite = _sprites[0];
     }
 
     public bool CanDestroy(ToolConfig config)
@@ -36,6 +46,16 @@ public class ResourceNode : MonoBehaviour
     public void TakeHit(float damage)
     {
         _currentHealth -= damage;
+
+        int lengthOfSpritesArr = _sprites.Length;
+
+        float increments = _nodeMaxHealth / lengthOfSpritesArr;
+
+        if(_currentHealth < _nodeMaxHealth - (increments * _currentSpriteIndex))
+        {
+            _currentSpriteIndex = (_currentSpriteIndex + 1) % lengthOfSpritesArr;
+            _renderer.sprite = _sprites[_currentSpriteIndex];
+        }
 
         if (_currentHealth <= 0.0f)
         {
